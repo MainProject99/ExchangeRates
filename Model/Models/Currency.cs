48930e7acd584f5ff6fc649bc6499c1e0336ffa1
Nick Tkachuk <tmikola11@gmail.com>
2019-12-20 22:17:17 +0200

    DB

---
+++ Model/Models/Currency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string CurrencyTo { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection <CurrencyFrom> CurrencyFroms { get; set; }
+
+        public Currency ()
+        {
+            CurrencyFroms = new List<CurrencyFrom>();
+        }
+    }
+}
---
+++ Model/Models/CurrencyFrom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Models
+{
+    public class CurrencyFrom
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Currency Currency { get; set; }
+    }
+}
